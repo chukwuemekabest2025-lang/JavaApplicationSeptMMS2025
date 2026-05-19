853708643df9542f5e82833d21229bd31ce5695e
chukwuemekabest2025-lang <chukwuemekabest2025@gmail.com>
2026-05-19 16:21:40 +0100

    Selections

---
+++ SingleSelection.java
@@ -0,0 +1,25 @@
+public class SingleSelection{
+	public static void main(String[] args){
+		int age = 21;
+		
+		if (age >= 18){
+			System.out.printf("Oh wow, you are %d years old and you are now an adult%n",age);
+			System.out.println("I am happy to be an adult");
+		
+			
+			
+			
+			
+			
+			
+			
+			
+// Control structure are used to manage the flow of execution based on certain conditions or loops.
+// In other words, control structure are rules that control the order in which statements in a program are executed.
+// Without them, java would just run code line by line from top to bottom.
+// Types are selection (decision making)
+	// Single Selection in java is used when you want to execute a block of code only if the condition is true
+// repitition (loop) and jump.
+		}
+	}
+}
