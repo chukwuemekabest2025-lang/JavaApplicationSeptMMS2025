8ae51ec4eb2c3034946dd766dbd7e4c91442a9a2
chukwuemekabest2025-lang <chukwuemekabest2025@gmail.com>
2026-06-15 15:54:35 +0100

    Array Classwork

---
+++ DimentionalArray.java
@@ -0,0 +1,8 @@
+public class SingleDimentionalArray{
+	public static void main(String[] args){
+		int num = 15;
+		int[] numbers = {2,4,6,8,12,15,20,25,26,30};
+		
+		System.out.printf("The element at index number 5 is %d%n",number[5]);
+	}
+}
