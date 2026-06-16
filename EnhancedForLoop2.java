96afb5dd3c6a842d3e39c3b5d3077aa4ec65558f
chukwuemekabest2025-lang <chukwuemekabest2025@gmail.com>
2026-06-16 16:00:10 +0100

    Enhanced For Loop

---
+++ EnhancedForLoop2.java
@@ -0,0 +1,13 @@
+public class EnhancedForLoop2{
+	public static void main(String[] args){
+		int[] marks = {10,20,30,40,50,60,70,80,90,100};
+		
+		System.out.println("Marks are");
+		for(int mark : marks){
+			System.out.printf("%d%n", mark);
+			
+		}
+	}
+}
+		
+		
