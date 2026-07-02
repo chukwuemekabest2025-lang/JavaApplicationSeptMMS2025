3ddf903042d5e67bd7b113e706aba080b005e93c
chukwuemekabest2025-lang <chukwuemekabest2025@gmail.com>
2026-07-02 15:53:09 +0100

    Using Queue

---
+++ UsingQueue.java
@@ -0,0 +1,31 @@
+import java.util.Queue;
+import java.util.LinkedList;
+
+
+public class UsingQueue{
+	public static void main(String[] args){
+		Queue<String> names = new LinkedList<>();
+		
+		
+		names.add("Henry");
+		names.add("Young");
+		names.add("John");
+		names.add("Peter");
+		
+		System.out.println(names);
+		System.out.println(names.poll());
+		System.out.println(names);
+		System.out.println(names.peek());
+		System.out.println(names);
+		System.out.printf("Is my queue empty? %b%n",names.isEmpty());
+		names.clear();
+		System.out.println(names);
+
+		names.add("Henry");
+		names.add("Young");
+		names.add("John");
+		names.add("Peter");
+		
+		System.out.println(names);
+	}
+}
